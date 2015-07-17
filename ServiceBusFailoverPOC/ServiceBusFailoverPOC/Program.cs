4ebd8ed7e6822cdc51ce7f78f40d22c3e3cde279
Ranger Yu <me@ranger.im>
2015-07-17 16:53:40 +0800

    start to have concurrent testing

--- ServiceBusFailoverPOC/ServiceBusFailoverPOC/Program.cs
+++ ServiceBusFailoverPOC/ServiceBusFailoverPOC/Program.cs
@@ -11,84 +11,22 @@ namespace ServiceBusFailoverPOC
     {
         static void Main(string[] args)
         {
-            // Please update the connection string before test.
-            string connStringMaster =
-                @"Endpoint=sb://ranger-ha-01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=UR/V+PT9H6WwenIm70hg+e9c3NZzs5ln8uqWqdtirns=";
-            string connStringSlave =
-                @"Endpoint=sb://ranger-ha-02.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=uJD7WqXtR+52/5IbuMqnZVToDK4zoSRKtCWbkynbuc8=";
+            //FailoverNormalSendRecvTest sendRecvTest = new FailoverNormalSendRecvTest();
+            //sendRecvTest.Start();
 
 
-            ServiceBusQueueManager<MessageTask> msgSender = new ServiceBusQueueManager<MessageTask>("fotest",
-                connStringMaster, connStringSlave);
+            
+        }
+    }
 
-            //Create two receiver clients to pull the msg.
-
-            ServiceBusQueueManager<MessageTask> msgReceiver = new ServiceBusQueueManager<MessageTask>("fotest",
-    connStringMaster, connStringSlave);
-
-            ServiceBusQueueManager<MessageTask> msgReceiver2 = new ServiceBusQueueManager<MessageTask>("fotest",
-connStringMaster, connStringSlave);
-
-            Console.WriteLine("Initilizing...");
-
-
-            // it is better to ensure the queues have been created already before running the application. although the queue manager will create if not exsits, in case each of Initialize() functions are called parallelly, lock issue will happen.
-            msgSender.Initialize();
-            msgReceiver.Initialize();
-            msgReceiver2.Initialize();
+    public class ConcurrentTest
+    {
+        public void Start()
+        {
+            
 
 
 
-            var senderTask = new Task(async () =>
-            {
-                int messageId = 0;
-
-                while (true)
-                {
-                    Task.Run(async () =>
-                    {
-                        Interlocked.Increment(ref messageId);
-
-                        await msgSender.SendMessageAsync(new MessageTask()
-                        {
-                            TaskId = (messageId).ToString(),
-                            TaskContent = "msgSender"
-                        });
-
-
-                    });
-                    
-                    await Task.Delay(50);
-                }
-            }, TaskCreationOptions.LongRunning);
-
-            var receiverTask = new Task(() =>
-            {
-                msgReceiver.StartReceiveMessages(async (msgTask,clientId) =>
-                {
-                    Console.WriteLine("MsgReceiver1, from {2}: ID={0} Content={1}", msgTask.TaskId, msgTask.TaskContent, clientId);
-                    msgTask.Status = TaskStatus.Completed;
-
-                });
-
-                msgReceiver2.StartReceiveMessages(async (msgTask, clientId) =>
-                {
-                    Console.WriteLine("MsgReceiver2, from {2}: ID={0} Content={1}", msgTask.TaskId, msgTask.TaskContent, clientId);
-                    msgTask.Status = TaskStatus.Completed;
-
-                });
-
-            }, TaskCreationOptions.LongRunning);
-
-            Console.WriteLine("Start Sending.");
-            senderTask.Start();
-
-            Console.WriteLine("Start Receiving.");
-            receiverTask.Start();
-
-            Console.ReadLine();
-            Console.WriteLine("Stopping...");
-            Task.WaitAll(msgSender.StopAsync(), msgReceiver.StopAsync(), msgReceiver2.StopAsync());
         }
     }
 
